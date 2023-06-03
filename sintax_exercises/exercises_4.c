c42ee6839544a70379b2d6130a30fcae20362e06
Eltorretaz <eltorretaz470@gmail.com>
2023-06-03 17:38:06 -0400

    modificacion del archivo exercises_4 y de texto

--- sintax_exercises/exercises_4.c
+++ sintax_exercises/exercises_4.c
@@ -3,12 +3,16 @@
 int main() {
   // Este es un comentario de una linea
 
-printf("\"hola mundo\""); //Este tambien es un comentario de una linea 
+printf("\"hello world oha yo sekai\""); //Este tambien es un comentario de una linea 
 
 /*
 este es 
 un comentario
 multilinea
+y esta linea 
+es para comprobra
+sobreescribir archivos
+en github
 */
   return 0;
 }  
